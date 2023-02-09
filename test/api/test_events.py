a9b8a85c922675688f44550264a30a06ad3683e9
estiven <esti-ven93@hotmail.com>
2023-02-09 12:05:34 -0500

    se creo el archivo de pruebas para la generacion del token

--- test/api/test_events.py
+++ test/api/test_events.py
@@ -4,3 +4,13 @@ from  api.api_v1.endpoints.events import router
 from fastapi.testclient import TestClient
 from fastapi import HTTPException
 
+client  = TestClient(router)
+
+@pytest.mark.parametrize("env_vars", [{"SECRET": "ZQeOwY6pNusL_xnUV_2i2g5F6BhYFaoL4mt9pB5ANw8="}])
+def test_create_event(env_vars) -> any:
+    os.environ.update(env_vars)
+    response = client.post(
+        f"/events", headers={
+                }
+    )
+    assert response.status_code == 400
